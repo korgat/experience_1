ecc60a3925f9c090ff73db8a027c753b491b7b4b
Taras Gasyuk <tarasgasyuk@gmail.com>
2024-05-22 17:56:43 +0200

    minor fix

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-// import 'app/styles/index.scss'
 
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
