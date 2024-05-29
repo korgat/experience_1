80c5af0102691bb60f051c7d880fae4d71c875bd
Taras Gasyuk <tarasgasyuk@gmail.com>
2024-05-29 18:13:39 +0200

    change sidebar add new props to Button

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import { Button, ThemeButton } from './Button';
+import { Button, ButtonSize, ButtonTheme } from './Button';
 
 export default {
     title: 'shared/Button',
@@ -23,18 +23,71 @@ Primary.args = {
 export const Clear = Template.bind({});
 Clear.args = {
     children: 'Text',
-    theme: ThemeButton.CLEAR,
+    theme: ButtonTheme.CLEAR,
 };
 
 export const Outline = Template.bind({});
 Outline.args = {
     children: 'Text',
-    theme: ThemeButton.OUTLINE,
+    theme: ButtonTheme.OUTLINE,
 };
 
 export const OutlineDark = Template.bind({});
 OutlineDark.args = {
     children: 'Text',
-    theme: ThemeButton.OUTLINE,
+    theme: ButtonTheme.OUTLINE,
 };
 OutlineDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const ButtonBackground = Template.bind({});
+ButtonBackground.args = {
+    children: 'Text',
+    theme: ButtonTheme.BACKGROUND,
+};
+
+export const ButtonBackgroundInverted = Template.bind({});
+ButtonBackgroundInverted.args = {
+    children: 'Text',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+};
+
+export const DefaultButton = Template.bind({});
+DefaultButton.args = {
+    children: 'Text',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+};
+export const DefaultButtonL = Template.bind({});
+DefaultButtonL.args = {
+    children: 'Text',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+    size: ButtonSize.L,
+};
+export const DefaultButtonXL = Template.bind({});
+DefaultButtonXL.args = {
+    children: 'Text',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+    size: ButtonSize.XL,
+};
+
+export const ButtonSquare = Template.bind({});
+ButtonSquare.args = {
+    children: '!',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+    square: true,
+};
+
+export const ButtonSquareL = Template.bind({});
+ButtonSquareL.args = {
+    children: '!',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+    square: true,
+    size: ButtonSize.L,
+};
+
+export const ButtonSquareXL = Template.bind({});
+ButtonSquareXL.args = {
+    children: '!',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+    square: true,
+    size: ButtonSize.XL,
+};
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,8 +2,12 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import MainPageIcon from 'shared/assets/icons/main-20-20.svg';
+import AboutPageIcon from 'shared/assets/icons/about-20-20.svg';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -23,15 +27,40 @@ export const Sidebar = ({ className }: SidebarProps) => {
             data-testid="sidebar"
             className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
+            <div className={cls.items}>
+                <AppLink
+                    className={cls.link}
+                    theme={AppLinkTheme.SECONDARY}
+                    to={RoutePath.main}
+                >
+                    <MainPageIcon />
+                    <span className={cls['link-text']}>{t('Главная')}</span>
+                </AppLink>
+                <AppLink
+                    className={cls.link}
+                    theme={AppLinkTheme.SECONDARY}
+                    to={RoutePath.about}
+                >
+                    <AboutPageIcon />
+                    <span className={cls['link-text']}>{t('О сайте')}</span>
+                </AppLink>
+            </div>
             <Button
+                className={cls['collapse-btn']}
+                theme={ButtonTheme.BACKGROUND_INVERTED}
+                square
+                size={ButtonSize.L}
                 data-testid="sidebar-toggle"
                 onClick={onToggle}
             >
-                {t('Переключить')}
+                {collapsed ? '>' : '<'}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                <LangSwitcher className={cls.lang} />
+                <LangSwitcher
+                    short={collapsed}
+                    className={cls.lang}
+                />
             </div>
         </div>
     );
