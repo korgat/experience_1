9ee2ed05af7ce201ccdb7cd132ff32e495070eb5
Taras Gasyuk <tarasgasyuk@gmail.com>
2025-03-19 17:02:44 +0100

    test

--- src/widgets/test.tsx
+++ src/widgets/test.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-import { cn } from '@/lib/utils'
+import { cn } from '@/lib/utils';
 
 interface TestProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Test = (props: TestProps) => {
-    const { className = '', ...rest } = props
+  const { className = '', ...rest } = props;
 
-    return (
-        <div {...rest} className={cn('', {}, [className])}>
-            Test
-        </div>
-    )
-}
+  return (
+    <div {...rest} className={cn('', {}, [className])}>
+      Test
+    </div>
+  );
+};
 
-export default Test
+export default Test;
